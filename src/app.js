1902d485d1f9760cf63088c372203dd985e3d37e
skobraf <skobraf@gmail.com>
2018-11-04 22:57:28 +0000

    added make decision button

--- src/app.js
+++ src/app.js
@@ -2,44 +2,58 @@ console.log("the app is running!");
 
 const app = {
 	title: 'Indecition',
-	subtitle: 'react app'
+	subtitle: 'react app',
+	options: []
 
 };
-const template =(
+const onRemoveAll = () => {
+	app.options = [];
+	render();
+};
+
+const onFormSubmit = (e) => {
+	e.preventDefault();
+
+	const option = e.target.elements.option.value;
+	if(option) {
+		app.options.push(option);
+		e.target.elements.option.value = '';
+		render();
+	}
+};
+
+const onMakeDecision = () => {
+	const randomNum = Math.floor(Math.random * app.options.length);
+	alert(app.options[randomNum]);
+}
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+	const template =(
 	<div>
 		<h1>{app.title}</h1>
 		{app.subtitle && <p>subtitle: {app.subtitle}</p>}
 		<p>{app.options.length > 0 ? "here are some options" : 'no options'}</p>
+		<button disabled={app.options.length === 0} onClick={onMakeDecision} >what should i do?</button>
+		<button onClick={on.onRemoveAll}>Remove all</button>
 		<ol>
-			<li>Item one</li>
-			<li>Item two</li>
+			{
+				app.options.map((option) => {
+				return <li key={option}>{option}</li>
+			})
+			}
 		</ol>
-	</div>
-	);
-let count = 0;
-const addOne = () => {
-	count++;
-	return renderCounterApp();
-};
-const minusOne = () => {
-	count--;
-	return renderCounterApp();
-};
-const reset = () => {
-	count = 0;
-};
-
-const appRoot = document.getElementById('app');
-const renderCounterApp = () => {
-	const templateTwo = (
-	<div>
-		<h1>Count: {count}</h1>
-		<button onClick={addOne}>+1</button>
-		<button onClick={minusOne}>+1</button>
-		<button onClick={reset}>reset</button>
+		<form onSubmit={onFormSubmit}>
+			<input type="text" name="option" />
+			<button>Add option</button>
+		</form>
 	</div>
 	);
 	ReactDOM.render(template, appRoot);
-};
+}
 
-renderCounterApp();
+
+render();
+
+
