b1fe02622c4540155f7054fd69dbe9b95bfc4666
skobraf <skobraf@gmail.com>
2018-11-08 16:47:42 +0000

    intorduced to setState

--- src/playground/counter.js
+++ src/playground/counter.js
@@ -1,3 +1,47 @@
+class Counter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleReset = this.handleReset.bind(this);
+		this.handleAddOne = this.handleAddOne.bind(this);
+		this.handleMinusOne = this.handleMinusOne.bind(this);
+		this.state = {
+			count:0
+		}
+	}
+	handleAddOne() {
+		this.setState((prevState) => {
+			return (
+				count: prevState.count +1
+				);
+		});
+
+	}
+	handleMinusOne() {
+		this.setState((prevState) => {
+			return (
+				count: prevState.count -1
+				);
+		});
+	}
+	handleReset() {
+		this.setState(() => {
+			return (
+				count:0
+				);
+		});
+	}
+	render() {
+		return (
+			<div>
+				<h1>Count: </h1>
+				<button onClick={this.handleAddOne}>+1</button>
+				<button onClick={this.handleMinusOne}>-1</button>
+				<button onClick={this.handleReset}>Reset</button>
+			</div>
+			)
+	}
+}
+/*
 let count = 0;
 const addOne = () => {
 	count++;
@@ -23,3 +67,6 @@ const renderCounterApp = () => {
 };
 
 renderCounterApp();
+
+*/
+
--- src/playground/visible-toggle.js
+++ src/playground/visible-toggle.js
@@ -1,4 +1,35 @@
 
+class VisibilityToggle extends React.Component {
+	constructor(props) {
+		super(props);
+		this.showDetails = this.showDetails.bind(this);
+		this.state = {
+			toggle: false
+		}
+		
+	}
+	showDetails() {
+		this.setState((prevState) => {
+			toggle:!prevState.toggle
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Visiti Toggle</h1>
+				<button onClick={this.VisibilityToggle}>{this.state.toggle ? "Hide details" :}</button>
+				{this.state.toggle && <p>these are the details</p>}
+			</div>
+			);
+
+	};
+
+
+}
+	ReactDOM.render(<VisibilityToggle />, getElementById('app'));
+
+/*
 const appRoot = document.getElementById("app");
 const toggle = false;
 const showDetails = () => {
@@ -15,4 +46,4 @@ const render = () => {
 ReactDOM.render(template, appRoot);
 }
 
-render();
+render();*/
